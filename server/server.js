9cbdb1581205eeb06f25b63dc25250cd82b18923
abhishek0207 <abhishekahuja02@gmail.com>
2018-08-25 21:02:37 -0400

    added tests for all post requests of todo api

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.post('/todos', (req, res) => {
     todo.save().then((doc) => {
     res.send(doc)
     }, (e) => {
-        res.send(e)
+        res.status(400).send(e)
     })
 })
 
@@ -23,4 +23,6 @@ app.listen(3000, () => {
 })
 
 
-
+module.exports = {
+    app
+}
