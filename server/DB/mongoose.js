6a1a1b2f2ab264bedd92b726b97fd7d56bf8aaf6
abhishek0207 <abhishekahuja02@gmail.com>
2018-08-27 19:42:52 -0400

    deployment to heorku configured

--- server/DB/mongoose.js
+++ server/DB/mongoose.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://127.0.0.1:27017/TodoApp');
+let db = {
+    localhost: 'mongodb://127.0.0.1:27017/TodoApp',
+    mlab: 'mongodb://abhishekahuja02:mickey_123@ds133622.mlab.com:33622/mytodoapp'
+}
+mongoose.connect(db.mlab);
 
 module.exports = {
     mongoose
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./DB/mongoose');
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
-
+const port = process.env.PORT || 3000; 
 var app = express();
 app.use(bodyParser.json());
 
@@ -39,8 +39,8 @@ app.get('/todos/:id', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('server is up');
+app.listen(port, () => {
+    console.log(`server is up at ${port}`);
 })
 
 
